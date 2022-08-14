89a53e51f64137770c972a906bb495c6c044965c
Gai Zomer <zomerg@gmail.com>
2022-08-14 16:57:04 +0300

    Fixed bug in the dict structure

--- Knesset Committees final.py
+++ Knesset Committees final.py
@@ -21,6 +21,7 @@ titles = ['חברי הוועדה: ', 'חברי הכנסת:', 'חבר הוועד�
 
 # function that the input is the name of the local file and the name of the title of the relevant section in the text. The function finds the title in each file and starts writing the lines after until a blank line. The output is a dictionary with ther name of the file, name of the title and list of the lines after the title.
 def name_list_f(f_file, titles):
+    protocol_dicts_list = []
     protocol_dict = {}
     protocol_dict['file'] = f_file
     doc = Document(f_file)
@@ -34,11 +35,15 @@ def name_list_f(f_file, titles):
             colleting_names = True
             names = []
             current_title = p.text
+
             continue  # no need to continue the loop
         if p.text == '' and colleting_names:
-            # finished collecting names
-            protocol_dict['title'] = current_title  # put the names in the dict
-            protocol_dict['names'] = names
+            # finished collecting names - save to dictionary
+            d = {}
+            d['file'] = f_file
+            d['title'] = current_title
+            d['names'] = names
+            protocol_dicts_list.append(d)
             # reset names and continue
             names = []
             colleting_names = False
@@ -46,14 +51,15 @@ def name_list_f(f_file, titles):
             # we are in collecting names mode
             names.append(p.text)
 
-    return protocol_dict
+    return protocol_dicts_list
 
 
-# running the function on a list of files and titles  and write all the dicts on a list (each object in the list is dict with 3 keys: file, tittle, list of names/lines)
+
+
+# running the function on a list of files and titles and write all the dicts on a list (each object in the list is dict with 3 keys: file, tittle, list of names/lines)
 for file in files:
 
-    all_lists.append(name_list_f(file, titles))
-
+    all_lists += name_list_f(file, titles)
 
 
 import csv
@@ -66,6 +72,7 @@ with open('PM_protocol_final.csv', 'w', newline='', encoding='utf-8') as output_
     dict_writer = csv.DictWriter(output_file, keys)
     dict_writer.writeheader()
     dict_writer.writerows(to_csv)
+    output_file.close()
 
 
 
