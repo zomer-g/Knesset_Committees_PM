e11d56963f80b2d709b0b8ae92b3393606ab6572
Gai Zomer <zomerg@gmail.com>
2022-08-14 15:49:20 +0300

    Change the dict structure and add writing to csv part

--- Knesset Committees final.py
+++ Knesset Committees final.py
@@ -1,5 +1,5 @@
 #create list of local files
-files_list2 = open("downloads/urls.txt", "r")
+files_list2 = open("urls.txt", "r")
 
 files = []
 for line in files_list2:
@@ -16,7 +16,7 @@ files_list2.close()
 from docx import Document
 
 all_lists = []
-titles = ['חברי הוועדה: ', 'חברי הכנסת:']
+titles = ['חברי הוועדה: ', 'חברי הכנסת:', 'חבר הוועדה: ', 'חבר הכנסת:', 'חברת הוועדה: ', 'חברת הכנסת:','חברות הוועדה: ', 'חברות הכנסת:']
 
 
 # function that the input is the name of the local file and the name of the title of the relevant section in the text. The function finds the title in each file and starts writing the lines after until a blank line. The output is a dictionary with ther name of the file, name of the title and list of the lines after the title.
@@ -37,7 +37,8 @@ def name_list_f(f_file, titles):
             continue  # no need to continue the loop
         if p.text == '' and colleting_names:
             # finished collecting names
-            protocol_dict[current_title] = names  # put the names in the dict
+            protocol_dict['title'] = current_title  # put the names in the dict
+            protocol_dict['names'] = names
             # reset names and continue
             names = []
             colleting_names = False
@@ -50,8 +51,24 @@ def name_list_f(f_file, titles):
 
 # running the function on a list of files and titles  and write all the dicts on a list (each object in the list is dict with 3 keys: file, tittle, list of names/lines)
 for file in files:
+
     all_lists.append(name_list_f(file, titles))
-print ('The end')
+
+
+
+import csv
+
+to_csv = all_lists
+
+keys = to_csv[0].keys()
+
+with open('PM_protocol_final.csv', 'w', newline='', encoding='utf-8') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(to_csv)
+
+
+
 
 
 
